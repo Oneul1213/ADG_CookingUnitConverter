5c5a66a04e09b5310db4661e276155c8d895d8a3
Oneul <toda1213@naver.com>
2022-09-03 17:40:31 +0900

    팁 계산 - 연습하기

--- app/src/main/java/com/example/cookingunitconverter/MainActivity.kt
+++ app/src/main/java/com/example/cookingunitconverter/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.cookingunitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cookingunitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convertButton.setOnClickListener { convert() }
+        binding.millisToOzSwitch.setOnCheckedChangeListener { compoundButton, isChecked ->
+            convert()
+        }
+        binding.gramsToCupSwitch.setOnCheckedChangeListener { compoundButton, b ->
+            convert()
+        }
+    }
+
+    private fun convert() {
+        val millisOrGram = binding.millisOrGram.text.toString().toDoubleOrNull()
+
+        if (millisOrGram == null || millisOrGram == 0.0) {
+            displayConvertResult(0.0)
+        } else {
+            displayConvertResult(millisOrGram)
+        }
+    }
+
+    private fun displayConvertResult(text: Double) {
+        val millisOrOz: String = if (binding.millisToOzSwitch.isChecked) getString(R.string.oz, (text * 0.03).toString())
+        else getString(R.string.millis, text.toString())
+
+        binding.millisOrGramTextView.text = millisOrOz
+
+        val gramOrCup = if (binding.gramsToCupSwitch.isChecked) getString(R.string.cup, (text * 128).toString())
+        else getString(R.string.gram, text.toString())
+
+        binding.gramOrCupTextView.text = gramOrCup
     }
 }
